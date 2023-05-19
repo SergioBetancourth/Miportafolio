551af3377c01a9a56be4ad0b5b1cf9e5d595e3d0
Sergio-Betancourth <alejandrobetancourth74@hotmail.com>
2023-05-19 07:34:15 -0500

    quary

--- js/media-query.js
+++ js/media-query.js
@@ -1,14 +1,13 @@
-let menuBar = document. getElementById("menuBar")
-let mainNav = document. getElementById("mainNav")
-let mainMenu = document. getElementById("mainMenu")
+let menuBar = document.getElementById("menuBar");
+let mainMenu = document. getElementById("mainMenu");
 
-menuBar.addEventListener("click", function(){
-    if(mainMenu.classList.contains ("main-menu")){
-    mainMenu.classList.add("main-menu-block")
-    mainMenu.classList.remove("main-menu")
+menuBar.addEventListener("click", function() {
+    if (mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
     }
-    else{
-     mainMenu.classList.add("main-menu")
-     mainMenu.classList.remove("main-menu-block")
+    else {
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-block");
     }
 });

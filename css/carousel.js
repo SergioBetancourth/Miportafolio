9f5b88548ffe9683af39eaeb3c58d15caaacabd3
Sergio-Betancourth <alejandrobetancourth74@hotmail.com>
2023-09-22 07:21:40 -0500

    commit

---
+++ css/carousel.js
@@ -0,0 +1,6 @@
+let boxCrousel = document.getElementById ("boxCarousel")
+boxCrousel.addEventListener ("mouseover", function(){
+    if (boxCrousel.classList.icontains("box-carousel-1")){
+        boxCarousel.classlist.remove
+    }
+})

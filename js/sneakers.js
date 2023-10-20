81cc49bf9acdb0cbb3448a539c48bd7e7fd42f00
Sergio-Betancourth <alejandrobetancourth74@hotmail.com>
2023-10-20 07:13:08 -0500

    sneakers

---
+++ js/sneakers.js
@@ -0,0 +1,7 @@
+let mainImg = document.getElementById("mainImg")
+let iconLeft =document.getElementById("iconLeft")
+let iconRight =document.getElementById("iconRight")
+
+iconLeft.addEventListener("click",function (){
+    alert("Preoductos du calidad du BRAZIL");
+});

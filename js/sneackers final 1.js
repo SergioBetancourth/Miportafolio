0e400fdecc66db2feaa880f8f340b0151eea84d2
Sergio-Betancourth <alejandrobetancourth74@hotmail.com>
2023-10-27 07:10:36 -0500

    sneak

--- js/sneackers final 1.js
+++ js/sneackers final 1.js
@@ -1,35 +1,26 @@
 let mainImg = document.getElementById("mainImg");
-let boxImgFixed = document.getElementById("boxImgFixed");
+let boxImgFixed=document.getElementById("boxImgFixed");
 let iconLeft = document.getElementById("iconLeft");
 let iconRight = document.getElementById("iconRight");
 let mainImgFixed1 = document.getElementById("mainImgFixed1");
 let mainImgFixed2 = document.getElementById("mainImgFixed2");
 let mainImgFixed3 = document.getElementById("mainImgFixed3");
 let mainImgFixed4 = document.getElementById("mainImgFixed4");
-let thumb1 = document.getElementById("thumb1");
-let thumb2 = document.getElementById("thumb2");
-let thumb3 = document.getElementById("thumb3");
-let thumb4 = document.getElementById("thumb4");
-let botonCerrar = document.getElementById("botonCerrar");
-/* let numeroImagen = 0; */
+let thumb1=document.getElementById("thumb1");
+let thumb2=document.getElementById("thumb2");
+let thumb3=document.getElementById("thumb3");
+let thumb4=document.getElementById("thumb4"); 
+let botonCerrar= document.getElementById("botonCerrar")
 
-mainImg.addEventListener("click", function() {
+botonCerrar.addEventListener("click",function() {
+    boxImgFixed.classList.add("box-img-fixed-hidden");
+    boxImgFixed.classList.remove("box-img-fixed");
+});
+mainImg.addEventListener("click",function() {
     boxImgFixed.classList.remove("box-img-fixed-hidden");
     boxImgFixed.classList.add("box-img-fixed");
-    /* mainImgFixed1.classList.remove(""); */
-/*         mainImgFixed1.classList.add("img-hidden");
-        mainImgFixed2.classList.remove("img-hidden");
-    }
-    else if (!mainImgFixed2.classList.contains("img-hidden")) {
-        mainImgFixed2.classList.add("img-hidden");
-        mainImgFixed3.classList.remove("img-hidden");
-    }
-    else if (!mainImgFixed3.classList.contains("img-hidden")) {
-        mainImgFixed3.classList.add("img-hidden");
-        mainImgFixed4.classList.remove("img-hidden");
-    } */
-    
 });
+
 iconRight.addEventListener("click", function() {
     if (!mainImgFixed1.classList.contains("img-hidden")) {
         mainImgFixed1.classList.add("img-hidden");
@@ -59,39 +50,32 @@ iconLeft.addEventListener("click", function() {
         mainImgFixed1.classList.remove("img-hidden");
     }
 });
-thumb1.addEventListener("click", function() {
-    mainImg.setAttribute("src", "img/image-product-1.jpeg");
+thumb1.addEventListener("click", function(){
+    mainImg.setAttribute("src","img/image-product-1.jpeg");
     mainImgFixed1.classList.remove("img-hidden");
     mainImgFixed2.classList.add("img-hidden");
     mainImgFixed3.classList.add("img-hidden");
     mainImgFixed4.classList.add("img-hidden");
-});
-thumb2.addEventListener("click", function() {
-    mainImg.setAttribute("src", "img/image-product-2.jpeg");
+})
+thumb2.addEventListener("click", function(){
+    mainImg.setAttribute("src","img/image-product-2.jpeg");
     mainImgFixed1.classList.add("img-hidden");
     mainImgFixed2.classList.remove("img-hidden");
     mainImgFixed3.classList.add("img-hidden");
     mainImgFixed4.classList.add("img-hidden");
-});
-thumb3.addEventListener("click", function() {
-    mainImg.setAttribute("src", "img/image-product-3.jpeg");
+})
+thumb3.addEventListener("click", function(){
+    mainImg.setAttribute("src","img/image-product-3.jpeg");
     mainImgFixed1.classList.add("img-hidden");
     mainImgFixed2.classList.add("img-hidden");
     mainImgFixed3.classList.remove("img-hidden");
     mainImgFixed4.classList.add("img-hidden");
-});
-thumb4.addEventListener("click", function() {
-    mainImg.setAttribute("src", "img/image-product-4.jpeg");
+})
+thumb4.addEventListener("click", function(){
+    mainImg.setAttribute("src","img/image-product-4.jpeg");
     mainImgFixed1.classList.add("img-hidden");
     mainImgFixed2.classList.add("img-hidden");
     mainImgFixed3.classList.add("img-hidden");
     mainImgFixed4.classList.remove("img-hidden");
 });
 
-/* Cerrar el cuadro modal */
-botonCerrar.addEventListener("click", function() {
-    boxImgFixed.classList.add("box-img-fixed-hidden");
-    boxImgFixed.classList.remove("box-img-fixed");
-});
-/* let fecha = new Date();
-alert(fecha.getDate().toString() + " " +fecha.getFullYear().toString()); */
